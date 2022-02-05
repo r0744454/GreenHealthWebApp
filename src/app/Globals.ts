71a78910bca2c036f206bcd7d7f55d1e2450ed5a
Wilfer Spaepen <39066186+wispae@users.noreply.github.com>
2022-02-05 18:27:01 +0100

    Update Globals.ts
    Do this so that maybe workflow will actually run?

--- src/app/Globals.ts
+++ src/app/Globals.ts
@@ -1,4 +1,5 @@
 export class ConnectionStrings {
+  
     public static apiUrl: string = "https://greenhealthapi.azurewebsites.net/api/";
     public static blobUrl: string = "https://storagemainfotosplanten.blob.core.windows.net/greenhealth/";
 }
